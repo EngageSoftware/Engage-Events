6efb3e43bf2b34ab3732e652ac9815eee487b95a
Brian Dukes <bdukes@engagesoftware.com>
2012-01-03 21:47:03 +0000

    Remove IUpgradeable that was only for DNN 4
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Events;C16891

--- Components/FeaturesController.cs
+++ Components/FeaturesController.cs
@@ -29,7 +29,7 @@ namespace Engage.Dnn.Events.Components
     /// Controls which DNN features are available for this module.
     /// </summary>
     [UsedImplicitly, SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Instantiated through reflection by DNN")]
-    internal class FeaturesController : IUpgradeable, ISearchable
+    internal class FeaturesController : ISearchable
     {
 #if TRIAL
         /// <summary>
@@ -37,25 +37,6 @@ namespace Engage.Dnn.Events.Components
         /// </summary>
         public static readonly Guid ModuleLicenseKey = new Guid("2A2C5DE3-8690-4D97-B027-4750409DAC9A");
 #endif
-
-        /// <summary>
-        /// Performs an action when the module is installed/upgraded, based on the given <paramref name="version"/>.
-        /// </summary>
-        /// <param name="version">The version to which the module is being upgraded.</param>
-        /// <returns>A status message</returns>
-        public string UpgradeModule(string version)
-        {
-            var versionNumber = new Version(version);
-            if (versionNumber.Equals(new Version(1, 4, 0)))
-            {
-                // this should only occur for DNN 4, the DNN 5 manifest doesn't setup the Event Queue to call UpgradeModule
-                // and if it does need to call UpgradeModule in the future, it shouldn't include 1.4.0 in the version list
-                // because we can add permissions declaratively through the DNN 5 manifest
-                return PermissionsService.CreateCustomPermissions();
-            }
-
-            return "No upgrade action required for version " + version + " of Engage: Events";
-        }
 
         /// <summary>
         /// Gets the search items for the given module.
