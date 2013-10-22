034cfb958b49ab9f3a8edb5fb92fbab403e7e5e6
Joshua Bradley <jbradley@engagesoftware.com>
2013-10-22 17:55:11 +0000

    Upping the File Version after changes yesterday.
    git-tfs-work-item:5553 associate
    git-tfs-code-reviewer:JB
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage 3.0/Engage.Events;C24644

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("3c3a937d-8a8e-4f12-bfd6-8f0d9453e505")]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("3.4.10.*")]
-[assembly: AssemblyFileVersion("3.4.10.0")]
+[assembly: AssemblyVersion("3.4.11.*")]
+[assembly: AssemblyFileVersion("3.4.11.0")]
